b7436ec54139062df9627931e5d13b6f318acee7
Katie Sallows <ktsallows@gmail.com>
2022-02-27 17:00:07 -0500

    added more animations

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-run = () => {
-    gsap.from(['.intro', '.big1', '.big2'], {
-        duration: 1,
-        opacity: 0,
-        x: 100
-    })
-}
+gsap.registerPlugin(ScrollTrigger);
 
-document.onload(run());
+gsap.from(['.intro', '.big1', '.big2'], {
+    duration: 1,
+    opacity: 0,
+    x: 200
+})
+
+gsap.utils.toArray('.inRight').forEach(element => {
+    gsap.from(element, {
+        scrollTrigger: {
+            trigger: element,
+            markers: true,
+            start: 'top center'
+        },
+        duration: 1,
+        x: 500
+    });
+});
+
+gsap.utils.toArray('.inLeft').forEach(element => {
+    gsap.from(element, {
+        scrollTrigger: {
+            trigger: element,
+            markers: true,
+            start: 'top center'
+        },
+        duration: 1,
+        x: -500
+    });
+});
