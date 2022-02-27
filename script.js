b1c50b2bf2601852fa82b970b260a039c19dc54b
Katie Sallows <ktsallows@gmail.com>
2022-02-27 18:32:51 -0500

    projects section

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(['.intro', '.big1', '.big2'], {
-    duration: 1,
-    opacity: 0,
-    x: 200
+let text = ['.intro', '.big1', '.big2']
+
+text.forEach((element, index) => {
+    gsap.from(element, {
+        duration: 1,
+        opacity: 0,
+        x: (index + 1) * 150
+    })
+    console.log(index);
 })
 
 gsap.utils.toArray('.inRight').forEach(element => {
     gsap.from(element, {
         scrollTrigger: {
             trigger: element,
-            markers: true,
             start: 'top center'
         },
         duration: 1,
+        opacity: 0,
         x: 500
     });
 });
@@ -22,10 +27,10 @@ gsap.utils.toArray('.inLeft').forEach(element => {
     gsap.from(element, {
         scrollTrigger: {
             trigger: element,
-            markers: true,
             start: 'top center'
         },
         duration: 1,
+        opacity: 0,
         x: -500
     });
 });
